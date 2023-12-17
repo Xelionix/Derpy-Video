31cba4f537ec656ccd8b0cf626d07f377cfe1b08
Gamma Ray <145352304+Xelionix@users.noreply.github.com>
2023-12-17 16:45:36 -0500

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// firebase-config.js
+const firebaseConfig = {
+  apiKey: "AIzaSyAbcSYBkJfHsuq9IJGGZylth8W7Nyu7Ths",
+  authDomain: "derpy-video.firebaseapp.com",
+  projectId: "derpy-video",
+  storageBucket: "derpy-video.appspot.com",
+  messagingSenderId: "40310817276",
+  appId: "1:40310817276:web:4dff2b1f4ea472a7f7b279",
+  measurementId: "G-48GDWWQJSS"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
